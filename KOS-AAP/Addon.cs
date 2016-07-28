f4daa05a5ddf345f8ac8521e78bcc33e39a114be
Christian Bronk <chris.github@bronk.de>
2016-07-28 17:25:53 +0200

    initial import

---
+++ KOS-AAP/Addon.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using kOS;
+using kOS.Safe;
+using kOS.Suffixed;
+using UnityEngine;
+using AtmosphereAutopilot.UI;
+using AtmosphereAutopilot;
+using KOS_AAP;
+
+using kOS.Safe.Encapsulation;
+
+namespace kOS.AddOns.KOSAAP
+{
+    [kOSAddon("AAP")]
+    [kOS.Safe.Utilities.KOSNomenclature("AAPAddon")]
+    public class Addon : Suffixed.Addon
+    {
+        internal AapControler koscontroler = new AapControler(AtmosphereAutopilot.AtmosphereAutopilot.Instance);
+        public Addon(SharedObjects shared) : base(shared)
+        {
+            InitializeSuffixes();
+        }
+
+        private void InitializeSuffixes()
+        {
+            AddSuffix(new[] { "SETFLIGHTMODE", "SETFM" }, new kOS.Safe.Encapsulation.Suffixes.OneArgsSuffix<BooleanValue, StringValue>(SetFlightMode, "Set the Flight Mode"));
+            AddSuffix("LISTMODES", new kOS.Safe.Encapsulation.Suffixes.NoArgsSuffix<ListValue>(ListFlightModes, "..."));
+            AddSuffix("CURRENTMODE", new kOS.Safe.Encapsulation.Suffixes.NoArgsSuffix<StringValue>(GetFlightMode, "..."));
+
+            AddSuffix("MD_SETDIR", new kOS.Safe.Encapsulation.Suffixes.OneArgsSuffix<BooleanValue, Vector>(MDSetDir, "..."));
+
+            AddSuffix("CC_SETMODE", new kOS.Safe.Encapsulation.Suffixes.OneArgsSuffix<BooleanValue, StringValue>(CCSetMode, "..."));
+            AddSuffix("CC_SETWP", new kOS.Safe.Encapsulation.Suffixes.OneArgsSuffix<BooleanValue, GeoCoordinates>(CCSetWp, "..."));
+            AddSuffix("CC_DELWP", new kOS.Safe.Encapsulation.Suffixes.NoArgsVoidSuffix(koscontroler.removeWaypoint, "..."));
+            AddSuffix("CC_SETALT", new kOS.Safe.Encapsulation.Suffixes.OneArgsSuffix<ScalarValue>(CCSetAltitude, "..."));
+            AddSuffix("CC_SETVERTICAL", new kOS.Safe.Encapsulation.Suffixes.OneArgsSuffix<ScalarValue>(CCSetVertical, "..."));
+
+
+            //           AddSuffix("COMPLETEDSCANS", new kOS.Safe.Encapsulation.Suffixes.TwoArgsSuffix<ListValue, BodyTarget, GeoCoordinates>(GetScans, "..."));
+            //           AddSuffix("ALLSCANTYPES", new kOS.Safe.Encapsulation.Suffixes.NoArgsSuffix<StringValue>(GetScanNames, "..."));
+            //           AddSuffix("ALLRESOURCES", new kOS.Safe.Encapsulation.Suffixes.NoArgsSuffix<ListValue>(GetResourceNames, "..."));
+            //           AddSuffix("RESOURCEAT", new kOS.Safe.Encapsulation.Suffixes.VarArgsSuffix<ScalarDoubleValue, Structure>(GetResourceByName, "..."));
+            //           AddSuffix("SLOPE", new kOS.Safe.Encapsulation.Suffixes.TwoArgsSuffix<ScalarDoubleValue, BodyTarget, GeoCoordinates>(GetSlope, "..."));
+            //           AddSuffix("GETCOVERAGE", new kOS.Safe.Encapsulation.Suffixes.TwoArgsSuffix<ScalarDoubleValue, BodyTarget, StringValue>(GetCoverage, "..."));
+        }
+
+        public override BooleanValue Available()
+        {
+            return true;
+        }
+
+
+
+        #region Listings
+        public ListValue ListFlightModes()
+        {
+            ListValue fmnames = new ListValue();
+            foreach (string fmname in Enum.GetNames(typeof(Autopilots)))
+            {
+                fmnames.Add(new StringValue(fmname));
+            }
+            return fmnames;
+        }
+
+
+        public StringValue GetFlightMode()
+        {
+            StringValue fm = new StringValue();
+            fm = new StringValue(koscontroler.currentAutopilot.ToString());
+            return fm;
+        }
+
+        public BooleanValue SetFlightMode(StringValue mode)
+        {
+            
+            koscontroler.currentAutopilot = GetFmByName(mode);
+            return true;
+        }
+
+        internal static Autopilots GetFmByName(string modename)
+        {
+            modename.ToUpper();
+            Autopilots pilot = Autopilots.DISABLED;
+            pilot= (Autopilots)Enum.Parse(typeof(Autopilots), modename);
+            return pilot;
+           }
+
+        #endregion
+        #region Mousedir
+        public BooleanValue MDSetDir (Vector dir )
+        {
+            if (koscontroler.currentAutopilot == Autopilots.MOUSEDIR)
+            {
+                koscontroler.dcAP.tgt_direction = dir.ToVector3D();
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+        #endregion
+        #region CruiseControl
+
+        internal static CruiseController.CruiseMode GetCCModeByName(string modename)
+        {
+            modename.ToUpper();
+            CruiseController.CruiseMode mode = CruiseController.CruiseMode.LevelFlight;
+            mode = (CruiseController.CruiseMode)Enum.Parse(typeof(CruiseController.CruiseMode), modename);
+            return mode;
+        }
+
+        public BooleanValue CCSetMode(StringValue mode)
+        {
+            if (koscontroler.currentAutopilot == Autopilots.CRUISECTRL)
+            {    
+                koscontroler.ccAP.current_mode = GetCCModeByName(mode);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public BooleanValue CCSetWp (GeoCoordinates coordinate)
+        {
+            if (koscontroler.currentAutopilot == Autopilots.CRUISECTRL)
+            {
+                koscontroler.ccAP.current_waypt.latitude = coordinate.Latitude;
+                koscontroler.ccAP.current_waypt.longtitude = coordinate.Longitude;
+                koscontroler.ccAP.waypoint_entered = true;
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        public void CCSetAltitude (ScalarValue altitude)
+        {
+            koscontroler.ccAP.height_mode = CruiseController.HeightMode.Altitude;
+            koscontroler.ccAP.desired_altitude.Value = Convert.ToSingle (altitude.Value);
+            koscontroler.ccAP.vertical_control = true;
+        }
+        public void CCSetVertical(ScalarValue v_velocity)
+        {
+            koscontroler.ccAP.height_mode = CruiseController.HeightMode.VerticalSpeed;
+            koscontroler.ccAP.desired_vertspeed.Value = Convert.ToSingle(v_velocity.Value);
+            koscontroler.ccAP.vertical_control = true;
+        }
+
+
+        #endregion
+    }
+}
---
+++ KOS-AAP/aapcontroler.cs
@@ -0,0 +1,381 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AtmosphereAutopilot;
+using UnityEngine;
+
+namespace KOS_AAP
+{
+    // shamlessly copied from AAP
+    class AapControler
+    {
+        private AtmosphereAutopilot.AtmosphereAutopilot parent;
+
+        internal TopModuleManager masterAP = null;
+        internal StandardFlyByWire fbwAP = null;
+        internal CruiseController ccAP = null;
+        internal DirectionController dcAP = null;
+        internal ProgradeThrustController speedAP = null;
+        internal PitchAngularVelocityController pvc = null;
+        internal YawAngularVelocityController yvc = null;
+
+        private void findModules()
+        {
+            if (parent.ActiveVessel == null || parent.autopilot_module_lists.ContainsKey(parent.ActiveVessel) == false)
+                return;
+
+            if (parent.autopilot_module_lists[parent.ActiveVessel].ContainsKey(typeof(TopModuleManager)))
+                masterAP = parent.autopilot_module_lists[parent.ActiveVessel][typeof(TopModuleManager)] as TopModuleManager;
+            if (parent.autopilot_module_lists[parent.ActiveVessel].ContainsKey(typeof(StandardFlyByWire)))
+                fbwAP = parent.autopilot_module_lists[parent.ActiveVessel][typeof(StandardFlyByWire)] as StandardFlyByWire;
+            if (parent.autopilot_module_lists[parent.ActiveVessel].ContainsKey(typeof(CruiseController)))
+                ccAP = parent.autopilot_module_lists[parent.ActiveVessel][typeof(CruiseController)] as CruiseController;
+            if (parent.autopilot_module_lists[parent.ActiveVessel].ContainsKey(typeof(DirectionController)))
+                dcAP = parent.autopilot_module_lists[parent.ActiveVessel][typeof(DirectionController)] as DirectionController;
+            if (parent.autopilot_module_lists[parent.ActiveVessel].ContainsKey(typeof(ProgradeThrustController)))
+                speedAP = parent.autopilot_module_lists[parent.ActiveVessel][typeof(ProgradeThrustController)] as ProgradeThrustController;
+            if (parent.autopilot_module_lists[parent.ActiveVessel].ContainsKey(typeof(PitchAngularVelocityController)))
+                pvc = parent.autopilot_module_lists[parent.ActiveVessel][typeof(PitchAngularVelocityController)] as PitchAngularVelocityController;
+            if (parent.autopilot_module_lists[parent.ActiveVessel].ContainsKey(typeof(YawAngularVelocityController)))
+                yvc = parent.autopilot_module_lists[parent.ActiveVessel][typeof(YawAngularVelocityController)] as YawAngularVelocityController;
+        }
+
+        public AapControler(AtmosphereAutopilot.AtmosphereAutopilot parent)
+        {
+            this.parent = parent;
+
+            findModules();
+        }
+
+        public bool appButtonOn
+        {
+            get
+            {
+                return parent.launcherButtonState;
+            }
+        }
+
+        public AtmosphereAutopilot.UI.Autopilots currentAutopilot
+        {
+            get
+            {
+                findModules();
+                if (masterAP != null && masterAP.Active == false)
+                    return AtmosphereAutopilot.UI.Autopilots.DISABLED;
+                if (fbwAP != null && fbwAP.Active)
+                    return AtmosphereAutopilot.UI.Autopilots.FLYBYWIRE;
+                if (ccAP != null && ccAP.Active)
+                    return AtmosphereAutopilot.UI.Autopilots.CRUISECTRL;
+                if (dcAP != null && dcAP.Active)
+                    return AtmosphereAutopilot.UI.Autopilots.MOUSEDIR;
+                return AtmosphereAutopilot.UI.Autopilots.DISABLED;
+            }
+
+            set
+            {
+                if (masterAP == null)
+                    return;
+
+                switch (value)
+                {
+                    case AtmosphereAutopilot.UI.Autopilots.DISABLED:
+                        fbwAP = null;
+                        ccAP = null;
+                        dcAP = null;
+                        speedAP = null;
+                        pvc = null;
+                        yvc = null;
+                        masterAP.Active = false;
+                        parent.setLauncherOnOffIcon(false);
+                        return;
+                    case AtmosphereAutopilot.UI.Autopilots.FLYBYWIRE:
+                        fbwAP = masterAP.activateAutopilot(typeof(StandardFlyByWire)) as StandardFlyByWire;
+                        ccAP = null;
+                        dcAP = null;
+                        break;
+                    case AtmosphereAutopilot.UI.Autopilots.CRUISECTRL:
+                        fbwAP = null;
+                        ccAP = masterAP.activateAutopilot(typeof(CruiseController)) as CruiseController;
+                        dcAP = null;
+                        break;
+                    case AtmosphereAutopilot.UI.Autopilots.MOUSEDIR:
+                        fbwAP = null;
+                        ccAP = null;
+                        dcAP = masterAP.activateAutopilot(typeof(DirectionController)) as DirectionController;
+                        break;
+                }
+                if (parent.autopilot_module_lists[parent.ActiveVessel].ContainsKey(typeof(ProgradeThrustController)))
+                    speedAP = parent.autopilot_module_lists[parent.ActiveVessel][typeof(ProgradeThrustController)] as ProgradeThrustController;
+                if (parent.autopilot_module_lists[parent.ActiveVessel].ContainsKey(typeof(PitchAngularVelocityController)))
+                    pvc = parent.autopilot_module_lists[parent.ActiveVessel][typeof(PitchAngularVelocityController)] as PitchAngularVelocityController;
+                if (parent.autopilot_module_lists[parent.ActiveVessel].ContainsKey(typeof(YawAngularVelocityController)))
+                    yvc = parent.autopilot_module_lists[parent.ActiveVessel][typeof(YawAngularVelocityController)] as YawAngularVelocityController;
+
+                parent.setLauncherOnOffIcon(masterAP.Active);
+            }
+        }
+
+        #region Speed Control
+
+        public bool speedControl
+        {
+            get
+            {
+                return speedAP != null && speedAP.spd_control_enabled;
+            }
+
+            set
+            {
+                if (speedAP != null)
+                    speedAP.spd_control_enabled = value;
+            }
+        }
+
+        public float speed
+        {
+            get
+            {
+                if (speedAP == null)
+                    return 0f;
+                return speedAP.setpoint.mps();
+            }
+
+            set
+            {
+                if (parent.ActiveVessel != null)
+                    speedAP.setpoint = new SpeedSetpoint(SpeedType.MetersPerSecond, value, parent.ActiveVessel);
+            }
+        }
+
+        #endregion
+
+        #region Fly-By-Wire
+
+        public bool rocketMode
+        {
+            get
+            {
+                return fbwAP != null && fbwAP.rocket_mode;
+            }
+            set
+            {
+                if (fbwAP != null)
+                    fbwAP.rocket_mode = value;
+            }
+        }
+
+        // the "normal" value for all the limits, hardcoded
+        private const float moderationNorm = 10f;
+
+        private void setModerationLimits(float limit)
+        {
+            if (pvc != null)
+            {
+                pvc.max_aoa = limit;
+                pvc.max_g_force = limit;
+            }
+            if (yvc != null)
+            {
+                yvc.max_aoa = limit;
+                yvc.max_g_force = limit;
+            }
+        }
+
+        public bool moderation
+        {
+            get
+            {
+                return fbwAP != null && fbwAP.moderation_switch;
+            }
+            set
+            {
+                if (fbwAP != null)
+                    fbwAP.moderation_switch = value;
+            }
+        }
+
+        public float moderationMult
+        {
+            get
+            {
+                if (pvc == null)
+                    return 1f;
+                //take pitch aoa limit as the master
+                float value = pvc.max_aoa;
+                setModerationLimits(value);
+                return value / moderationNorm;
+            }
+            set
+            {
+                setModerationLimits(value * moderationNorm);
+            }
+        }
+
+        #endregion
+
+        #region Cruise Control
+
+        public bool altitudeControl
+        {
+            get
+            {
+                return ccAP != null && ccAP.vertical_control;
+            }
+            set
+            {
+                if (ccAP != null)
+                    ccAP.vertical_control = value;
+            }
+        }
+
+        public float altitude
+        {
+            get
+            {
+                if (ccAP == null)
+                    return 0f;
+                return ccAP.desired_altitude;
+            }
+            set
+            {
+                if (ccAP != null)
+                    ccAP.desired_altitude.Value = value;
+            }
+        }
+
+        public float distToWaypoint
+        {
+            get
+            {
+                if (ccAP == null || ccAP.current_mode != CruiseController.CruiseMode.Waypoint && ccAP.waypoint_entered)
+                    return -1f;
+
+                return (float)ccAP.dist_to_dest;
+            }
+        }
+
+
+        public bool waypointIsSet
+        {
+            get
+            {
+                return ccAP != null && ccAP.current_mode == CruiseController.CruiseMode.Waypoint && ccAP.waypoint_entered ;
+            }
+        }
+
+        public void removeWaypoint()
+        {
+            if (ccAP == null)
+                return;
+            ccAP.picking_waypoint = false;
+            ccAP.LevelFlightMode = true;
+        }
+
+        #endregion
+
+    }
+}
+namespace AtmosphereAutopilot
+{
+    public sealed class DirectionController : StateController
+    {
+        internal DirectionController(Vessel v)
+            : base(v, "Direction Director", 88437228)
+        { }
+
+        public FlightModel imodel;
+        //AccelerationController acc_c;
+        public DirectorController dir_c;
+        public ProgradeThrustController thrust_c;
+
+        public override void InitializeDependencies(Dictionary<Type, AutopilotModule> modules)
+        {
+            imodel = modules[typeof(FlightModel)] as FlightModel;
+            dir_c = modules[typeof(DirectorController)] as DirectorController;
+            thrust_c = modules[typeof(ProgradeThrustController)] as ProgradeThrustController;
+        }
+
+        protected override void OnActivate()
+        {
+            dir_c.Activate();
+            thrust_c.Activate();
+            MessageManager.post_status_message("Direction Director enabled");
+        }
+
+        protected override void OnDeactivate()
+        {
+            dir_c.Deactivate();
+            thrust_c.Deactivate();
+            MessageManager.post_status_message("Direction Director disabled");
+        }
+
+        public override void ApplyControl(FlightCtrlState cntrl)
+        {
+            if (vessel.LandedOrSplashed)
+                return;
+
+
+            dir_c.ApplyControl(cntrl, tgt_direction, Vector3d.zero);
+
+            if (thrust_c.spd_control_enabled)
+                thrust_c.ApplyControl(cntrl, thrust_c.setpoint.mps());
+        }
+
+        public Vector3 tgt_direction;
+
+        public override void OnUpdate()
+        {
+           
+        }
+
+        [AutoGuiAttr("Director controller GUI", true)]
+        protected bool DirGUI { get { return dir_c.IsShown(); } set { if (value) dir_c.ShowGUI(); else dir_c.UnShowGUI(); } }
+
+        [AutoGuiAttr("Thrust controller GUI", true)]
+        protected bool PTCGUI { get { return thrust_c.IsShown(); } set { if (value) thrust_c.ShowGUI(); else thrust_c.UnShowGUI(); } }
+
+        protected override void _drawGUI(int id)
+        {
+            close_button();
+            GUILayout.BeginVertical();
+            AutoGUI.AutoDrawObject(this);
+            thrust_c.SpeedCtrlGUIBlock();
+            GUILayout.EndVertical();
+            GUI.DragWindow();
+        }
+
+        public class CenterIndicator : MonoBehaviour
+        {
+            Material mat = new Material(Shader.Find("KSP/Sprite"));
+
+            Vector3 startVector = new Vector3(0.494f, 0.5f, -0.001f);
+            Vector3 endVector = new Vector3(0.506f, 0.5f, -0.001f);
+
+            public bool enabled = false;
+
+            public void OnPostRender()
+            {
+                if (enabled)
+                {
+                    GL.PushMatrix();
+                    mat.SetPass(0);
+                    mat.color = Color.red;
+                    GL.LoadOrtho();
+                    GL.Begin(GL.LINES);
+                    GL.Color(Color.red);
+                    GL.Vertex(startVector);
+                    GL.Vertex(endVector);
+                    GL.End();
+                    GL.PopMatrix();
+                    enabled = false;
+                }
+            }
+        }
+
+
+    }
+
+
+}
